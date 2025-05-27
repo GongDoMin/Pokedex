2c6f550bd7473a0fd6c26ecd55e7e0d50ebf0936
GongDoMin <yhw198@naver.com>
2025-05-27 23:56:58 +0900

    pokemonEntity, pokemonLocalEntity 의존관계 추가

--- core/local/src/main/java/co/kr/mvisample/local/model/PokemonLocalEntity.kt
+++ core/local/src/main/java/co/kr/mvisample/local/model/PokemonLocalEntity.kt
@@ -1,9 +1,20 @@
 package co.kr.mvisample.local.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "pokemon-local")
+@Entity(
+    tableName = "pokemon-local",
+    foreignKeys = [
+        ForeignKey(
+            entity = PokemonEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class PokemonLocalEntity(
     @PrimaryKey val id: Int = 0,
     val iconUrl: String = "",
