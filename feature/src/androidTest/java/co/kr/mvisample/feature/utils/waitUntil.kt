e8829b1c46d6f0185f51a0d013566a86b8a9cb0e
GongDoMin <yhw198@naver.com>
2025-06-08 22:18:27 +0900

    waitUntil fun 추가

---
+++ feature/src/androidTest/java/co/kr/mvisample/feature/utils/waitUntil.kt
@@ -0,0 +1,21 @@
+package co.kr.mvisample.feature.utils
+
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import co.kr.mvisample.testing.HiltTestActivity
+
+fun AndroidComposeTestRule<ActivityScenarioRule<HiltTestActivity>, HiltTestActivity>.waitUntil(
+    node: SemanticsNodeInteraction,
+    action: (SemanticsNodeInteraction) -> Unit = {},
+    timeout: Long = 3_000L
+) {
+    this.waitUntil(
+        timeoutMillis = timeout,
+        condition = {
+            node.isDisplayed()
+        }
+    )
+    action(node)
+}
