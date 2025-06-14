b3647b6e47b350abdb9255cce2ec4f3342ef520c
GongDoMin <yhw198@naver.com>
2025-06-14 16:51:12 +0900

    DetailScreenTest 수정

--- core/testing/src/main/java/co/kr/mvisample/testing/utils/waitUntilAssert.kt
+++ core/testing/src/main/java/co/kr/mvisample/testing/utils/waitUntilAssert.kt
@@ -1,12 +1,36 @@
 package co.kr.mvisample.testing.utils
 
 import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.SemanticsNodeInteractionCollection
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 
 fun AndroidComposeTestRule<*, *>.waitUntilAssert(
     node: AndroidComposeTestRule<*, *>.() -> SemanticsNodeInteraction,
     assert: SemanticsNodeInteraction.() -> Unit,
     action: SemanticsNodeInteraction.() -> Unit = {},
+    timeoutMillis: Long = 2_000L
+) {
+    val targetNode = this.node()
+
+    waitUntil (
+        timeoutMillis = timeoutMillis,
+        condition = {
+            try {
+                targetNode.assert()
+                true
+            } catch (e: AssertionError) {
+                false
+            }
+        }
+    )
+
+    targetNode.action()
+}
+
+fun AndroidComposeTestRule<*, *>.waitUntilAssertAll(
+    node: AndroidComposeTestRule<*, *>.() -> SemanticsNodeInteractionCollection,
+    assert: SemanticsNodeInteractionCollection.() -> Unit,
+    action: SemanticsNodeInteractionCollection.() -> Unit = {},
     timeoutMillis: Long = 2_000L
 ) {
     val targetNode = this.node()
