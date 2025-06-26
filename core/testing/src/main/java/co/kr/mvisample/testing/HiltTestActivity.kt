fecade07b3ab44711fca42995e129570b68a0d2d
GongDoMin <yhw198@naver.com>
2025-06-26 22:50:26 +0900

    FakeImageLoaderEngine intercept 추가

--- core/testing/src/main/java/co/kr/mvisample/testing/HiltTestActivity.kt
+++ core/testing/src/main/java/co/kr/mvisample/testing/HiltTestActivity.kt
@@ -1,12 +1,15 @@
 package co.kr.mvisample.testing
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import co.kr.mvisample.feature.R
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
 import coil3.annotation.DelicateCoilApi
+import coil3.asImage
 import coil3.test.FakeImageLoaderEngine
 import coil3.test.default
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,8 +21,15 @@ class HiltTestActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val iconDrawable = ContextCompat.getDrawable(this, R.drawable.img_charizard_icon)
+            ?: error("icon Drawable not found")
+        val drawable = ContextCompat.getDrawable(this, R.drawable.img_charizard)
+            ?: error("Drawable not found")
+
         val engine = FakeImageLoaderEngine.Builder()
-            .default(ContextCompat.getDrawable(applicationContext, R.drawable.img_charizard_icon) ?: error("drawable is null"))
+            .intercept({ it is String && it.startsWith("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/") }, iconDrawable.toBitmap(config = Bitmap.Config.ARGB_8888).asImage())
+            .intercept({ it is String && it.startsWith("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/gold/transparent/") }, drawable.toBitmap(config = Bitmap.Config.ARGB_8888).asImage())
+            .default(drawable)
             .build()
 
         val imageLoader = ImageLoader.Builder(applicationContext)
