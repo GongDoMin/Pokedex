07736c7ecb7782bf0d1033c6155586f6aad6008a
GongDoMin <yhw198@naver.com>
2025-07-02 19:13:23 +0900

    enableEdgeToEdge 적용

--- app/src/main/java/co/kr/mvisample/ui/MainActivity.kt
+++ app/src/main/java/co/kr/mvisample/ui/MainActivity.kt
@@ -1,14 +1,32 @@
 package co.kr.mvisample.ui
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowInsetsControllerCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
+
+        WindowInsetsControllerCompat(
+            window,
+            window.decorView
+        ).apply {
+            isAppearanceLightNavigationBars = false
+            isAppearanceLightStatusBars = false
+        }
+
         setContent {
             PokemonApp()
         }
