7581cd05924324fa839e677be265ed376625e925
GongDoMin <yhw198@naver.com>
2025-07-02 19:40:33 +0900

    Detail 화면 수정

--- feature/src/main/java/co/kr/mvisample/feature/detail/Detail.kt
+++ feature/src/main/java/co/kr/mvisample/feature/detail/Detail.kt
@@ -73,11 +73,7 @@ fun DetailScreen(
         onDismissDialog = { viewModel.handleBasicDialogAction(DialogAction.BasicDialogAction.OnDismissDialog) },
         onClickPositiveButton = { viewModel.handleBasicDialogAction(DialogAction.BasicDialogAction.OnClickPositiveButton) }
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(PokemonTheme.colors.backgroundRed)
-        ) {
+        Column {
             DetailHeader(
                 pokemonDetail = uiState.content.pokemonDetail,
                 onBackClick = { viewModel.handleAction(DetailAction.OnBackClick) }
