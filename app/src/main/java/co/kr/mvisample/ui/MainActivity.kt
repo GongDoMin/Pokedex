0d8848616eecf564e761f7f2bb68c254b145f363
GongDoMin <yhw198@naver.com>
2025-07-03 13:02:18 +0900

    enabledEdgeToEdge navigationBarStyle 추가

--- app/src/main/java/co/kr/mvisample/ui/MainActivity.kt
+++ app/src/main/java/co/kr/mvisample/ui/MainActivity.kt
@@ -1,11 +1,12 @@
 package co.kr.mvisample.ui
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowInsetsControllerCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,19 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            window.isNavigationBarContrastEnforced = false
-        }
-
-        WindowInsetsControllerCompat(
-            window,
-            window.decorView
-        ).apply {
-            isAppearanceLightNavigationBars = false
-            isAppearanceLightStatusBars = false
-        }
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
 
         setContent {
             PokemonApp()
